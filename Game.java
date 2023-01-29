9eaeb05f8be34e4f134f078472d5dfd996bc4627
Ido <ido722722@gmail.com>
2023-01-29 14:42:44 +0200

    Final final commit

--- Game.java
+++ Game.java
@@ -1,9 +1,9 @@
 package somethingInASomething1;
 
 public abstract class Game{
-	
-	public abstract void play(Participant p);
-	
+    
+    public abstract void play(Participant p);
+    
     public abstract void insert(Participant p);
 
     public abstract Participant checkWin();
--- Participant.java
+++ Participant.java
@@ -4,14 +4,14 @@ public class Participant{
     private String name;
     private int score;
     private char shape;
-	
-	public Participant(String name, int score, char shape){
-		this.name = name;
-		this.score = score;
-		this.shape = shape;
-	}
-	
-	public char getShape(){
-		return this.shape;
-	}
+    
+    public Participant(String name, int score, char shape){
+        this.name = name;
+        this.score = score;
+        this.shape = shape;
+    }
+    
+    public char getShape(){
+        return this.shape;
+    }
 }
